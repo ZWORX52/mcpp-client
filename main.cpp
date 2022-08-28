f36c6e6b53feafdf85e5455a1c4696b6a080f8d2
ZWORX52 <66217856+ZWORX52@users.noreply.github.com>
2022-08-28 19:13:22 -0400

    Yay! ..Oh no. my brain has fried officially now

--- main.cpp
+++ main.cpp
@@ -5,11 +5,23 @@ void error_callback(int error, const char *description) {
   fprintf(stderr, "GLFW Error #%d: %s\n", error, description);
 }
 
+// I'll be handling inputs in the tick thread for the user depending on the
+// current game state. (distant: ENGLISH, PLEASE!) Fine. Every tick, the client
+// will poll and process inputs in singleplayer. In multiplayer, the client will
+// send inputs every client tick. Happy? Good.
+// Note to self: use if (glfwGetKey(window, GLFW_KEY_<key name>)) { ... }
+/*
 static void key_callback(__attribute__((unused)) GLFWwindow *window, int key,
                          int scancode, int action, int modifiers) {
   std::cout << "Key: " << key << ", scancode: " << scancode
             << ", action: " << action << ", modifiers: " << modifiers
             << std::endl;
+}
+*/
+
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow *window,
+                               int width, int height) {
+  glViewport(0, 0, width, height);
 }
 
 int main() {
@@ -33,17 +45,25 @@ int main() {
     }
     return 1;
   }
-  glfwSetKeyCallback(window, key_callback);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
   glfwMakeContextCurrent(window);
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cout << "Failed to initialize OpenGL context" << std::endl;
     return 1;
   }
+  glfwSwapInterval(1);
+  glViewport(0, 0, 640, 480);
 
   uint64_t frames = 0;
   while (!glfwWindowShouldClose(window)) {
-    // Finally, a window we can use! Yay!
+    // Finally, a window we can use! Yay! *reads "Hello Triangle" on
+    // learnopengl.com* Oh no...
     frames++;
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
   }
 
   std::cout << "Stopping!" << std::endl;
